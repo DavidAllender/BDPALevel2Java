9a59ff02ee8e271fe845aafc953b60da500f65ea
David Allender <david.a.allender@gmail.com>
2018-04-06 18:46:59 -0500

    adding adventure game shell

---
+++ AdventureGameExample/src/main/java/com/example/Adventure/Game/AdventureGameApplication.java
@@ -0,0 +1,12 @@
+package com.example.Adventure.Game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureGameApplication.class, args);
+	}
+}
